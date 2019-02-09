f89ba18d8348f033c71970928e619984b4657df4
greseam <greseam@CND82405RT>
2019-02-08 20:41:37 -0600

    forgot to branch

--- project1/Display.cs
+++ project1/Display.cs
@@ -7,6 +7,7 @@ namespace project1
 {
     class Display
     {
+        private bool draftTF = true;
         List<Player> playerList = new List<Player>();
         public Display()
         {
@@ -14,9 +15,42 @@ namespace project1
         }
         public void run()
         {
+            while (draftTF == true)
+            {
+                DisplayTable();
+            }
+        }
+
+        public void DisplayTable()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                       // getPlayer(i, j).name;
+                       // getPlayer(i,j).price;
+                }
+            }
+        }
+
+        public void Welcome()
+        {
+
+        }
+
+        public void getPlayer(int i, int j)
+        {
             foreach (Player player in playerList)
             {
-                Console.WriteLine(player.name);
+                /*
+                if (player.position == playerList[i])
+                {
+                    if (player.pick== Convert.ToString(j))
+                    {
+                        return player;
+                    }
+                }
+                */
             }
         }
     }

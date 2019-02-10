c3a0203d232b170fbc12695b03d445737f66f88d
greseam <greseam@CND82405RT>
2019-02-10 16:04:55 -0600

    Developed more functuionality

--- project1/Display.cs
+++ project1/Display.cs
@@ -8,24 +8,23 @@ namespace project1
     class Display
     {
         private double budget = 95000000;
+        private double totalPrice = 0;
         
         List<Player> playerList = new List<Player>();
-        List<string> positionsList = new List<string>();
-        List<string> placementList = new List<string>();
         List<int> usedOptions = new List<int>();
+        List<int> verifyInts = new List<int>();
         private int End = 5;
         private int runOnce = 0;
         private double newBudget = 0;
-        private bool canCancel = true;
 
         int availableCheck = 0;
         const int startX = 17;
         const int startY = 3;
-        const int startY2 = 0;
-        const int startY3 = 1;
+        const int startY2 = 1;
+        const int startY3 = 2;
         const int optionsPerLine = 5;
         const int spacingPerLine = 20;
-        const int spacingByLine = 3;
+        const int spacingByLine = 2;
 
         int currentSelection = 0;
 
@@ -38,7 +37,7 @@ namespace project1
         }
         public void run()
         {
-            while (End >= 0 && canCancel == true)
+            while (End >= 0 )
             {
                 DisplayTable();
                 End--;
@@ -52,6 +51,8 @@ namespace project1
         public void DisplayTable()
         {
             Console.Clear();
+            Titles();
+            TitlesX();
             do
             {
                
@@ -90,7 +91,6 @@ namespace project1
                     Console.Write(playerList[i].name);
                     extraColums(startY2, playerList[currentSelection].school);
                     extraColums(startY3, playerList[currentSelection].price.ToString("C"));
-                    //extraColums();
                     Console.ResetColor();
                 }
 
@@ -127,29 +127,38 @@ namespace project1
                             break;
                         }
                 }
-            } while (key != ConsoleKey.Enter || availableCheck == 0);
+            } while ((key != ConsoleKey.Enter || availableCheck == 0) && End >= 0);
 
             runOnce = 0;
-            while (runOnce == 0)
+            while (runOnce == 0 && End >=0)
             {
                 Console.Clear();
-                Console.WriteLine("\n\n\t\tYou have selected " + playerList[currentSelection].name + ", has a salary of: " +
-                                  playerList[currentSelection].price.ToString("C")+" from "+playerList[currentSelection].school);
+                totalPrice = totalPrice + playerList[currentSelection].price;
                 budget = budget - playerList[currentSelection].price;
                 newBudget = budget;
-                Console.WriteLine("\nYour remaining budget is " + (newBudget).ToString("C") + " and you can pick " +
-                                  (End - 1) + " more players.");
-                if (newBudget >= 65000000 && End <= 2)
+                if (newBudget <= 0)
                 {
-                    Console.WriteLine("\nGreat job your selection is cost effective");
+                    budgetCheck();
                 }
 
-                Console.WriteLine("\nHit Escape to exit the program.\nHit any other key to coninue.");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\n\n\t\tYou have selected " + playerList[currentSelection].name + ", has a salary of: " +
+                                  playerList[currentSelection].price.ToString("C")+" from "+playerList[currentSelection].school);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n\t\tYour remaining budget is " + (newBudget).ToString("C") + " and you can pick " +
+                                  (End - 1) + " more players.");
+                Console.ResetColor();
+                Console.WriteLine("\n\nPress 'X' to verify selection\nHit any other key to coninue.\nHit Escape to exit the program.");
+                verifyInts.Add(currentSelection);
                 usedOptions.Add(currentSelection);
                 ConsoleKey Input = Console.ReadKey().Key;
                 if (Input == ConsoleKey.Escape)
                 {
                     checkESC();
+                }
+                else if (Input == ConsoleKey.X)
+                {
+                    verifySelcetion();
                 }
                 else
                 {
@@ -161,7 +170,7 @@ namespace project1
         public void Welcome()
         {
             Console.CursorVisible = false;
-            Console.WriteLine("Hello Welcome to the 2019 NFL Draft!!!\nTo start the draft hit a key that is not ESC.\nTo exit simply hit the 'ESC' key\n\n");
+            Console.WriteLine("Hello Welcome to the 2019 NFL Draft!!!\nTo select a player use your keyboards Arrow buttons and Enter.\n\tYour starting budget is 95 million dollars.\n\nTo start the draft hit a key that is not ESC.\nTo exit simply hit the 'ESC' key\n\n");
             ConsoleKey Input = Console.ReadKey().Key;
             if (Input == ConsoleKey.Escape)
             {
@@ -176,46 +185,20 @@ namespace project1
             }
         }
 
-        public Player GetPlayer(int i, int j)
-        {
-            var somVale = new Player();
-            managePositions();
-            foreach (Player player in playerList)
-            {
-                if (player.position == positionsList[j])
-                {
-                    //if (player.pick == pi)
-                   // {
-                       // somVale = player;
-                   // }
-
-                }
-                
-            }
-
-            return somVale;
-
-        }
-
-        public void managePositions()
-        {
-            Player player = new Player();
-            for (int i = 0; i < 5; i++)
-            {
-                positionsList.Add(player.position);
-            }
-            
-        }
-
         public void runAgain()
         {
             runOnce = 1;
             while (runOnce == 1)
             {
-
-                Console.Clear();
-                Console.WriteLine("You can not pick any more players\n");
-                //implement a efficent choice msg
+                if (End == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Clear();
+                    Console.WriteLine("You can not pick any more players\n");
+                    Console.ResetColor();
+                    verifySelcetion();
+                }
+                
                 Console.WriteLine("\nWould you like to draft again, but with different players? (Y/N)");
                 ConsoleKey tryAgain = Console.ReadKey().Key;
                 if (tryAgain == ConsoleKey.N)
@@ -229,16 +212,19 @@ namespace project1
                 else if (tryAgain == ConsoleKey.Y)
                 {
                     usedOptions.Clear();
+                    verifyInts.Clear();
                     Console.Clear();
                     End = 5;
                     budget = 95000000;
                     newBudget = 0;
+                    totalPrice = 0;
                     runOnce--;
                     break;
                 }
                 else
                 {
-
+                    Console.Clear();
+                    verifySelcetion();
                 }
             }
             
@@ -250,7 +236,9 @@ namespace project1
             while (runOnce == 1)
             {
                 Console.Clear();
-                Console.WriteLine("\nyou hit the Esc key, are you sure you would like to leave? (Y/N)");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nYou hit the Esc key, are you sure you would like to leave? (Y/N)");
+                Console.ResetColor();
                 ConsoleKey tryAgain = Console.ReadKey().Key;
                 if (tryAgain == ConsoleKey.N)
                 {
@@ -262,7 +250,6 @@ namespace project1
                     Console.Clear();
                     Console.WriteLine("Thank You for using the 2019 NFL draft.\nHave a nice day");
                     End = -1;
-                    canCancel = false;
                     runOnce--;
                 }
                 else
@@ -271,30 +258,124 @@ namespace project1
             }
         }
 
-        private void extraColums(int Y, string varPlayer)
+        private void extraColums(int Y,string var)
         {
             
                 for (int i = 0; i < playerList.Count; i++)
                 {
-                    Console.SetCursorPosition(1 , Y );
-                    Console.Write(varPlayer);
+                    Console.SetCursorPosition(1 , Y);
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write(var);
+                    Console.ResetColor();
                     Console.ResetColor();
                 }
             
         }
-        private void Titles(int Y,int X, string varPlayer)
+        private void Titles()
         {
-           
+            Console.ForegroundColor = ConsoleColor.Yellow;
             for (int i = 0; i < playerList.Count; i++)
             {
-                    Console.SetCursorPosition(X + (i % 5) * spacingPerLine,Y );
-                    Console.Write(varPlayer);
-                    Console.ResetColor();
-
+                Console.SetCursorPosition(1 , 3 + (i / optionsPerLine) * spacingByLine);
+                Console.WriteLine(playerList[0 + i % 5 * 8].position);
             }
-
+            Console.ResetColor();
         }
-       
+
+        private void TitlesX()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < playerList.Count; i++)
+            {
+                Console.SetCursorPosition(17 + (i % 5) * spacingPerLine, 1);
+                if (playerList[0 + i % 5 * 8].pick == "1")
+                {
+                    Console.WriteLine("Best");
+                }
+                else if (playerList[0 + i % 5 * 8].pick == "2")
+                {
+                    Console.WriteLine("Second");
+                }
+                else if (playerList[0 + i % 5 * 8].pick == "3")
+                {
+                    Console.WriteLine("Third");
+                }
+                else if (playerList[0 + i % 5 * 8].pick == "4")
+                {
+                    Console.WriteLine("Fourth");
+                }
+                else if (playerList[0 + i % 5 * 8].pick == "5")
+                {
+                    Console.WriteLine("Fifth");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            Console.ResetColor();
+        }
+        
+
+        private void budgetCheck()
+        {
+            runOnce = 1;
+            while (runOnce == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Clear();
+                Console.WriteLine("\nThe Player you tryed to select exceeded your budget!\nWould you like to try again? (Y/N)");
+                Console.ResetColor();
+                ConsoleKey tryAgain = Console.ReadKey().Key;
+                if (tryAgain == ConsoleKey.N)
+                {
+                    Console.Clear();
+                    runOnce--;
+                }
+                else if (tryAgain == ConsoleKey.Y)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Thank You for using the 2019 NFL draft.\nHave a nice day");
+                    End = 5;
+                    runOnce--;
+                }
+                else
+                {
+                }
+            }
+        }
+        void verifySelcetion()
+        {
+            End = -1;
+            Console.Clear();
+            Console.WriteLine("Here are your Selected players for your draft");
+            if (newBudget >= (95000000 - 65000000) && (playerList[currentSelection].pick == "1" || playerList[currentSelection].pick == "2" || playerList[currentSelection].pick == "3"))
+            {
+                Console.WriteLine("\nGreat job your selection is cost effective");
+            }
+            for (int i = 0; i < verifyInts.Count; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                 Console.SetCursorPosition(startX + (i % verifyInts.Count) * spacingPerLine,
+                startY );
+                 Console.WriteLine(playerList[i].name);
+                 Console.SetCursorPosition(startX + (i % verifyInts.Count) * spacingPerLine,
+                     startY+1);
+                 Console.WriteLine(playerList[i].school);
+                 Console.SetCursorPosition(startX + (i % verifyInts.Count) * spacingPerLine,
+                     startY+2);
+                 Console.WriteLine(playerList[i].price.ToString("C"));
+                 Console.ResetColor();
+                 Console.ForegroundColor = ConsoleColor.Yellow;
+                 Console.WriteLine("\n\nYour Remaining budget is " + newBudget.ToString("C"));
+                 Console.WriteLine("Your Draft total cost is " + totalPrice.ToString("C"));
+                 Console.ResetColor();
+                 
+            }
+            runAgain();
+            
+        }
+
     }
 
 
